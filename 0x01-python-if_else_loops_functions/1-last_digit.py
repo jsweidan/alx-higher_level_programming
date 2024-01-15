901301631bd108d0681d5e017e72f8aa56f7e0ab
jsweidan <jawaheryslam@gmail.com>
2024-01-15 06:03:38 -0800

    asssign a random number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ if lastdigit > 5:
     print(f"{thestring} and is greater than 5")
 elif lastdigit == 0:
 	print(f"{thestring} and is 0")
-elif lastdigit < 6 && != 0:
+elif lastdigit < 6:
     print(f"{thestring} and is less than 6 and not 0")
 
