119242e645757e7270102b5702936816a858717a
jsweidan <jawaheryslam@gmail.com>
2024-02-15 14:47:39 -0800

    lists all records of the table

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 --script that lists all records of the table of the database in your MySQL server
-SELECT hbtn_0c_0 FROM second_table
-ORDER BY hbtn_0c_0 ASC
-
+SELECT 'score', 'name'
+FROM second_table
+ORDER BY 'score' DESC;
