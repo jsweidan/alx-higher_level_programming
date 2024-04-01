0243088fd0d295732b58449ac2f418e8eb236698
jsweidan <jawaheryslam@gmail.com>
2024-04-01 04:51:02 -0700

    a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 #a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s [GET request-header-name request-header-value]
 curl -s [GET response-header-name: response-header-value]
+curl -s [POST response-header-name: response-header-value]
