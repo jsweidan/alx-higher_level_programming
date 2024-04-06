f18fca38b6455ba44ff2e047b3baf229d0e01803
jsweidan <jawaheryslam@gmail.com>
2024-04-06 15:23:22 -0700

    imports a function and prints the result

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0
+from add_0.py import add_0
 def add(a, b):
     a = 1;
     b = 2;
 
-    print(add_0.add(f"({a} + {b} = {a+b}) \n"))
+    print(add_0.add(f"({a} + {b} = {a+b})"))
