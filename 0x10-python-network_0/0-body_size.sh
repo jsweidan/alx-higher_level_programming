48826fe1171e4631deb635fdb27f32df3972f3b3
jsweidan <jawaheryslam@gmail.com>
2024-07-07 15:59:03 -0700

    URL script that sends request

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
-#a Bash script that takes in a URL, sends a request and displays the size of the response
+# a Bash script that takes in a URL, sends a request to that URL
+# and displays the size of the body of the response
 
-curl -s "$1" | wc -
+curl -s "$1" | wc -c
