4c32af37365fa998336abd516073ccfc409349de
jsweidan <142082659+jsweidan@users.noreply.github.com>
2024-07-25 01:29:05 +0300

    Update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,3 @@
 --script that lists all records of the table of the database in your MySQL server
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table 
+  ORDER BY score DESC;
